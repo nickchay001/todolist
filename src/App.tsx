72e644194043836f5df5c77b178f93b5d9a6e07b
Nick <119807798+nickchay001@users.noreply.github.com>
2023-06-12 15:50:29 +0300

    create Todolist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import './App.css';
+import s from './App.module.css'
+import { Todolist } from './Todolist';
+
+export type TasksType = {
+  id:number
+  title:string
+  isDone:boolean
+}
 
 function App() {
+ 
+  let tasks1:TasksType[] = [
+    {id:1, title:'CSS', isDone:true},
+    {id:2, title:'JS', isDone:true},
+    {id:3, title:'React', isDone:false},
+  ]
+
+  let tasks2:TasksType[] = [
+    {id:1, title:'Terminator', isDone:true},
+    {id:2, title:'XXX', isDone:false},
+    {id:3, title:'Jentelments of fortune', isDone:true},
+  ]
+
   return (
-    <div className="App">
-    
+    <div className={s.app}>
+      <Todolist tasks={tasks1} title='What to learn ?' />
+      <Todolist tasks={tasks2} title='Movies' />
     </div>
   );
 }
+
 
 export default App;
